0aef7c8af02eda8cb3a6d7cba4bd5bb5a3e0fef7
LachezarPanayotov <l4zrpanajotov@gmail.com>
2021-06-16 23:27:38 +0300

    final form

--- menu.py
+++ menu.py
@@ -1,24 +1,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDesktopWidget, QWidget
+import os
+
 
 class App(QMainWindow, QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Football quiz'
-        self.left = 60
-        self.top = 60
-        self.width = 640
-        self.height = 480
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        button = QPushButton('Logo quiz', self)
-        button.move(100, 70)
-        button = QPushButton('Football questions',self)
-        button.move(100, 100)
+        self.resize(640, 480)
         self.center()
+        self.setWindowTitle(self.title)
+        button = QPushButton('Logo quiz', self)
+        button.setGeometry(245, 120, 150, 40)
+        button.clicked.connect(self.on_click1)
+        button = QPushButton('Football questions', self)
+        button.setGeometry(245, 170, 150, 40)
+        button.clicked.connect(self.on_click2)
+        button = QPushButton('Scoreboard', self)
+        button.setGeometry(245, 220, 150, 40)
+        button.clicked.connect(self.on_click3)
         self.show()
 
     def center(self):
@@ -26,6 +29,19 @@ class App(QMainWindow, QWidget):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def on_click1(self):
+        ex.close()
+        os.system('LogoQuiz\\logo_quiz.py')
+
+    def on_click2(self):
+        ex.close()
+        os.system('football_questions.py')
+
+    def on_click3(self):
+        ex.close()
+        os.system('scoreboard.TXT')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
